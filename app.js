d138f89893902be6a65af8b1633bb76918f4c8cb
sabri.ben_miled <sabri.ben_miled@utt.fr>
2019-06-06 15:54:46 +0200

    start project

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import db from './db/db';// Set up the express app
+import bodyParser from 'body-parser';
+
+
+const app = express();
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// get all todos
+app.get('/api/v1/todos', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'todos retrieved successfully',
+        todos: db
+    })
+});
+
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+});
