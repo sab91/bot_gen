3078ba3addf4334abcd5bdd35f6cc76704eac4e1
sabri.ben_miled <sabri.ben_miled@utt.fr>
2019-06-09 16:32:43 +0200

    remove gitignore files

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import db from './db/db';// Set up the express app
 import bodyParser from 'body-parser';
+import AWS from 'aws-sdk'
 
 
 const app = express();
+
+const lexmodelbuildingservice = new AWS.LexModelBuildingService({ region: 'us-east-1'});
 
 // Parse incoming requests data
 app.use(bodyParser.json());
@@ -16,6 +19,69 @@ app.get('/api/v1/todos', (req, res) => {
         message: 'todos retrieved successfully',
         todos: db
     })
+});
+
+const params = {
+    name: "DocOrderPizzaBot",
+    abortStatement: {
+        messages: [
+            {
+                content: "I don't understand. Can you try again?",
+                contentType: "PlainText"
+            },
+            {
+                content: "I'm sorry, I don't understand.",
+                contentType: "PlainText"
+            }
+        ]
+    },
+    childDirected: true,
+    clarificationPrompt: {
+        maxAttempts: 1,
+        messages: [
+            {
+                content: "I'm sorry, I didn't hear that. Can you repeate what you just said?",
+                contentType: "PlainText"
+            },
+            {
+                content: "Can you say that again?",
+                contentType: "PlainText"
+            }
+        ]
+    },
+    description: "Orders a pizza from a local pizzeria.",
+    idleSessionTTLInSeconds: 300,
+    intents: [
+        {
+            intentName: "DocOrderPizza",
+            intentVersion: "$LATEST"
+        }
+    ],
+    locale: "en-US",
+    processBehavior: "SAVE"
+};
+
+// required params
+/*const params = {
+    name: "DocOrderPizzaBot",
+    childDirected: true,
+    locale: "en-US",
+};*/
+
+// Lex try
+app.post('/api/createBot', (req, res) => {
+    lexmodelbuildingservice.putBot(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);
+    });
+
+});
+
+app.delete('/api/deleteBot', (req, res) => {
+    lexmodelbuildingservice.putBot(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);
+    });
 });
 
 
