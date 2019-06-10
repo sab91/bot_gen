d65eefd0033d5f6345d9033fe43b19892a686aaf
NGUYEN Phuc <phucdenlqd@gmail.com>
2019-06-10 18:51:57 +0200

    fonctionalite create bots

--- controller/bots.controller.js
+++ controller/bots.controller.js
@@ -1,25 +1,61 @@
 import AWS from 'aws-sdk';
+import alert from 'alert-node'
+
 AWS.config.loadFromPath('./config/config.json');
-const lexmodelbuildingservice = new AWS.LexModelBuildingService({ region: 'us-east-1'});
+const lexmodelbuildingservice = new AWS.LexModelBuildingService({
+  region: 'us-east-1'
+});
 module.exports.index = (req, res) => {
-    let paramsGetBots={
-        maxResults:40,
-        nextToken:""
-      }
-      lexmodelbuildingservice.getBots(paramsGetBots, function(err, data) {
-          if (err) {
-            console.log(err, err.stack);
-          }else {
-            console.log(data);
-            res.render('bots');
-          }
-      });
+  let paramsGetBots = {
+    maxResults: 50,
+    nextToken: ""
+  }
+  lexmodelbuildingservice.getBots(paramsGetBots, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      console.log(data);
+      res.render('bots');
+    }
+  });
 
 }
 module.exports.create = (req, res) => {
-    res.render('createBots')
+  res.render('createBots')
 }
 module.exports.postCreate = (req, res) => {
-    req.body.date = new Date();
-    res.redirect('/bots')
+  let params = {
+    name: req.body.nom,
+    abortStatement: {
+      messages: [{
+        content: req.body.abandon,
+        contentType: "PlainText"
+      }]
+    },
+    childDirected: true,
+    clarificationPrompt: {
+      maxAttempts: 1,
+      messages: [{
+        content: req.body.clarification,
+        contentType: "PlainText"
+      }]
+    },
+    description: req.body.description,
+    idleSessionTTLInSeconds: 300,
+    locale: "en-US",
+    processBehavior: "SAVE"
+  };
+
+  // Lex try
+  lexmodelbuildingservice.putBot(params, function(err, data) {
+    if (err){
+      console.log(err, err.stack)
+      res.render('createBots',{errorMessage:err.message,params:params})
+    }else {
+      res.redirect('/bots')
+    }
+  });
+
+
+  console.log(req.body)
 }
